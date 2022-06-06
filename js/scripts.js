7a3f09d22614d453fe7e70d7243b36d064e6bac3
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-06 10:53:09 -0700

    add scripts to add answers to an array
    Co-authored-by: Manuel Raygoza  <manny.raygoza@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form#favthings").submit(function(event) {
+  event.preventDefault();
+  const answer1 = parseInt($("#input1").val())
+  const answer2 = $("#input2").val();
+  const answer3 = $("#input3").val();
+  const answer4 = $("#input4").val();
+  
+  event.preventDefault();
+  let array = [];
+  array.push(answer1, answer2, answer3, answer4);
+  console.log(array);
+  });
+});
+
